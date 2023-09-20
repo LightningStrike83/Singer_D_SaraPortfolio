0ee37885f88f093bf81ac918e13208c2597faa26
Dan Singer <d_singer@fanshaweonline.ca>
2023-09-19 23:21:31 -0400

    Index- Navigation
    Added content for the nav for the index

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log ('Express and Immerse')
+let workSelection = document.querySelectorAll ('.work')
+let workLinks = document.querySelector('.work-links')
+
+function showNav() {
+    workLinks.classList.add('show');
+}
+
+function hideNav() {
+    workLinks.classList.remove('show');
+};
+
+workSelection.forEach(work => work.addEventListener('mouseenter', showNav));
+workSelection.forEach(work => work.addEventListener('mouseleave', hideNav));
+
+//Appendix
+
+//Prompt: Hi, what's wrong with this code? -inserted code-
+//Return: You are trying to use forEach on workSelection, but it's a single DOM element, not a collection. To select multiple elements with the class 'work', you should use document.querySelectorAll('.work')
